87e484b74110d80c1be9c178f5c7963f7bd185f7
AsfourMohammad <mohammad.asfo@gmail.com>
2022-08-18 03:15:31 +0300

    Pro_2

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 //creat array of object movie
-const movie=[{imgMovie:"https://i.egycdn.com/pic/WmFwZndlY212bWJtRW1tbW1tcGFjdk5tYW1ibGxtdm1n.jpg",titleMovie:"Thor: Love and Thunder"},
-{imgMovie:"https://i.egycdn.com/pic/WmFwZndlY212bUVtbW1ZbW1wRUVjY3dhbWptam1Fd2Z3bA.jpg",titleMovie:"Top Gun: Maverick"}]
+const movie=[{imgMovie:"https://i.egycdn.com/pic/WmFwZndlY212bWJtRW1tbW1tcGFjdk5tYW1ibGxtdm1n.jpg"
+,titleMovie:"Thor: Love and Thunder"
+,type:"Fiction"},
+{imgMovie:"https://i.egycdn.com/pic/WmFwZndlY212bUVtbW1ZbW1wRUVjY3dhbWptam1Fd2Z3bA.jpg",
+titleMovie:"Top Gun: Maverick",
+type:"action"}]
 //select to div(main)
 const main=$("#main");
 //creat div (bar) in top app 
 const bar=$("<div>Movies</div>");
 main.append(bar)
 bar.addClass("bar");
-//creat div search in bar
-const search=$("<div><input>Search..</input><button>search</button></div>")
+//creat div search in bar  ins
+const search=$("<div><input></input><button>search</button></div>")
 search.addClass("search")
 bar.append(search)
 //creat button sort in bar 
-const sortBy=$("<button>Sort by</button>")
+const sortBy=$(`<select name="sort" >
+<option value="all">all sort</option>
+<option value="action">action</option>
+<option value="drama">drama</option>
+<option value="comedy">comedy</option>
+<option value="Historical">Historical</option>
+<option value="Fiction">Fiction</option>
+</select>`)
 bar.append(sortBy)
+
 //creat button Home in bar 
 const Home=$("<button>Home</button>")
 bar.append(Home)
@@ -28,13 +40,21 @@ index=0;
 const funMovie=()=>{
     //use map To view Movie
     movie.map((elem,i)=>{
-        const listMovie=$("<button></button>")
-        listMovie.text(elem);
+        const listMovie=$(`<div><img src="${elem.imgMovie}"/>${elem.titleMovie}</div>`)
         main.append(listMovie);
-       
+        listMovie.addClass("listMovie") 
+       index++
     })
 }
-console.log(funMovie());
+// console.log(funMovie());
+// sortBy.on("click",typeFun(e))
+// const typeFun=()=>{
+//     movie.filter((elem,i)=>{
+// return elem.type===e.target.value
+
+//     })
+
+// }
 
 
 
